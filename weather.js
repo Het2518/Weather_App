7835f85861892d326e9c603eeb0e30592106a9cb
Het Monpara <131437054+Het2518@users.noreply.github.com>
2024-03-10 19:22:26 +0530

    updated

--- weather.js
+++ weather.js
@@ -1,30 +1,11 @@
 const apikey = "160237aafff500f8b86c6f0ad7c384d5";
-const cache = {}; // Cache for city suggestions
-
-// Debounce function to limit the number of API calls
-function debounce(func, wait) {
-  let timeout;
-  return function executedFunction(...args) {
-    const later = () => {
-      clearTimeout(timeout);
-      func(...args);
-    };
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-  };
-}
 
 async function fetchCitySuggestions(query) {
-  if (cache[query]) {
-    return cache[query];
-  }
-
   const url = `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=10&appid=${apikey}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    cache[query] = data; // Cache the result
     return data;
   } catch (error) {
     console.error('Error fetching city suggestions:', error);
@@ -37,11 +18,15 @@ window.addEventListener("load", () => {
     navigator.geolocation.getCurrentPosition((position) => {
       let lon = position.coords.longitude;
       let lat = position.coords.latitude;
-      const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`;
+      const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon=${lon}&appid=${apikey}`;
 
       fetch(url)
-          .then((res) => res.json())
-          .then((data) => weatherReport(data));
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          weatherReport(data);
+        });
     });
   }
 });
@@ -50,8 +35,7 @@ const inputField = document.getElementById('input');
 const suggestionsList = document.createElement('ul');
 suggestionsList.classList.add('suggestions');
 
-// Debounce the input event to reduce the number of API calls
-inputField.addEventListener('input', debounce(async () => {
+inputField.addEventListener('input', async () => {
   const inputValue = inputField.value.trim();
 
   if (inputValue !== '') {
@@ -77,100 +61,7 @@ inputField.addEventListener('input', debounce(async () => {
   } else {
     inputField.parentNode.removeChild(suggestionsList);
   }
-}, 300)); // 300ms debounce time
-
-function searchByCity() {
-  var place = document.getElementById('input').value;
-  var urlsearch = `http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apikey}`;
-
-  fetch(urlsearch)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return res.json();
-      })
-      .then((data) => {
-        weatherReport(data);
-      })
-      .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
-      });
-  document.getElementById('input').value = '';
-}
-
-function weatherReport(data) {
-  var urlcast = `http://api.openweathermap.org/data/2.5/forecast?q=${data.name}&appid=${apikey}`;
-
-  fetch(urlcast)
-      .then((res) => res.json())
-      .then((forecast) => {
-        hourForecast(forecast);
-        dayForecast(forecast);
-
-        document.getElementById('city').innerText = data.name + ', ' + data.sys.country;
-        document.getElementById('temperature').innerText = Math.floor(data.main.temp - 273) + ' °C';
-        document.getElementById('clouds').innerText = data.weather[0].description;
-
-        let icon1 = data.weather[0].icon;
-        let iconurl = "http://api.openweathermap.org/img/w/" + icon1 + ".png";
-        document.getElementById('img').src = iconurl;
-      });
-}
-
-function hourForecast(forecast) {
-  document.querySelector('.templist').innerHTML = '';
-  for (let i = 0; i < 5; i++) {
-    var date = new Date(forecast.list[i].dt * 1000);
-
-    let hourR = document.createElement('div');
-    hourR.setAttribute('class', 'next');
-
-    let div = document.createElement('div');
-    let time = document.createElement('p');
-    time.setAttribute('class', 'time');
-    time.innerText = date.toLocaleTimeString(undefined, 'Asia/Kolkata').replace(':00', '');
-
-    let temp = document.createElement('p');
-    temp.innerText = Math.floor(forecast.list[i].main.temp_max - 273) + ' °C' + ' / ' + Math.floor(forecast.list[i].main.temp_min - 273) + ' °C';
-
-    div.appendChild(time);
-    div.appendChild(temp);
-
-    let desc = document.createElement('p');
-    desc.setAttribute('class', 'desc');
-    desc.innerText = forecast.list[i].weather[0].description;
-
-    hourR.appendChild(div);
-    hourR.appendChild(desc);
-    document.querySelector('.templist').appendChild(hourR);
-  }
-}
-
-function dayForecast(forecast) {
-  document.querySelector('.weekF').innerHTML = '';
-  for (let i = 8; i < forecast.list.length; i += 8) {
-    let div = document.createElement('div');
-    div.setAttribute('class', 'dayF');
-
-    let day = document.createElement('p');
-    day.setAttribute('class', 'date');
-    day.innerText = new Date(forecast.list[i].dt * 1000).toDateString(undefined, 'Asia/Kolkata');
-    div.appendChild(day);
-
-    let temp = document.createElement('p');
-    temp.innerText = Math.floor(forecast.list[i].main.temp_max - 273) + ' °C' + ' / ' + Math.floor(forecast.list[i].main.temp_min - 273) + ' °C';
-    div.appendChild(temp);
-
-    let description = document.createElement('p');
-    description.setAttribute('class', 'desc');
-    description.innerText = forecast.list[i].weather[0].description;
-    div.appendChild(description);
-
-    document.querySelector('.weekF').appendChild(div);
-  }
-}
-
+});
 
 function searchByCity() {
   var place = document.getElementById('input').value;
